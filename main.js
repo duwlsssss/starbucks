239209b07ccdcaca09851bdd913a694ee0bfa230
duwlsssss <rladuwls0814@gmail.com>
2024-10-08 11:33:47 +0900

    edit header search, add mobile hambuger bar

--- main.js
+++ main.js
@@ -2,8 +2,13 @@
 // 검색창 placeholder 표시
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
+
 searchEl.addEventListener('click',function(){
-  searchInputEl.focus();
+  const computedStyle = window.getComputedStyle(searchInputEl);
+  if (computedStyle.display === 'none') {
+    searchInputEl.style.display = 'block'; 
+    searchInputEl.focus(); 
+  }
 });
 searchInputEl.addEventListener('focus',function(){
   searchEl.classList.add('focused');
@@ -62,6 +67,53 @@ window.addEventListener('scroll', _.throttle(function(){
   }
 },300));
 
+// mobile__menu 표시
+const mobileMenuBtn = document.querySelector('.narrow-width-menu .hamburger-icon');
+const menubg = document.querySelector('.mobile__menu__back');
+const menu = document.querySelector('.mobile__menu');
+const closeBtn = document.querySelector('.close-mobile-menu');
+
+mobileMenuBtn.addEventListener('click', () => {
+  gsap.to(menubg,{
+    opacity:1,
+    display:'block',
+    duration:.6,
+  });
+  menu.classList.remove('mobile__menu--hide');
+  menu.classList.add('mobile__menu--show');
+});
+
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('mobile__menu--show');
+  menu.classList.add('mobile__menu--hide');
+  gsap.to(menubg,{
+    opacity:0,
+    display:'none',
+    duration:.6,
+  });
+});
+
+// mobile__menu 아코디언
+const categories = document.querySelectorAll('.categories__m .category');
+categories.forEach(category => {
+  const title = category.querySelector('.title');
+  title.addEventListener('click', () => {
+    const lists = category.querySelectorAll('.list');
+    lists.forEach(list => {
+      if (list.style.display === 'none' || list.style.display === '') {
+        list.style.display = 'block'; // 목록 보이기
+      } else {
+        list.style.display = 'none'; // 목록 숨기기
+      }
+    });
+    const arrow = title.querySelector('.arrow');
+    if(arrow.textContent==='arrow_drop_down'){
+      arrow.textContent = 'arrow_drop_up';
+    }else{
+      arrow.textContent = 'arrow_drop_down';
+    }
+  });
+});
 
 // visual 페이드인
 const fadelEls = document.querySelectorAll('.visual .fade-in').forEach((fadeEl,index) => {
