7800e85d50ac2d0aece86cc9fd9415849c0879bf
duwlsssss <rladuwls0814@gmail.com>
2024-10-11 19:50:49 +0900

    목록, 스크롤 애니메이션 추가

--- main.js
+++ main.js
@@ -123,3 +123,86 @@ const fadelEls = document.querySelectorAll('.visual .fade-in').forEach((fadeEl,i
     delay:index * .5, //순차적으로 표시
   });
 });
+
+// notice 공지사항 swiper
+new Swiper('.notice-line .swiper',{
+  direction: 'vertical',
+  autoplay: true,
+  loop: true,
+});
+
+// notice promition 토글 버튼
+let isOpenPromotion = false;
+const promotionEl = document.querySelector('.promotion');
+const promotionBtn = document.querySelector('.toggle-promotion');
+const promotionBtnIcon = promotionBtn.querySelector('.material-icons');
+promotionBtn.addEventListener('click', function(){
+  isOpenPromotion = !isOpenPromotion;
+  if (isOpenPromotion) {
+    // 보임
+    promotionEl.classList.remove('hide');
+    promotionBtnIcon.textContent = 'keyboard_arrow_up';
+  } else {
+    // 숨김
+    promotionEl.classList.add('hide');
+    promotionBtnIcon.textContent = 'keyboard_arrow_down';
+  }
+});
+
+// motice promition swiper
+new Swiper('.promotion .swiper',{
+  slidesPerView: 3,  
+  spaceBetween: 10,  
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination',
+    clickable: true,
+  },
+});
+
+// awards swiper
+new Swiper('.awards .swiper', {
+  autoplay: true, 
+  loop: true,
+  navigation: { 
+    prevEl: '.awards .swiper-prev', 
+    nextEl: '.awards .swiper-next',
+  },
+  spaceBetween: 10, 
+  slidesPerView: 2, 
+  breakpoints: {
+    600:{
+      spaceBetween: 20, 
+      slidesPerView: 3, 
+    },
+    900: {
+      slidesPerView: 4,  
+      spaceBetween: 30,
+    },
+    1200:{
+      slidesPerView: 5,  
+      spaceBetween: 40,
+    }
+  },
+})
+
+// 스크롤 위치에 따하 요소가 화면에 보여짐 여부 관리 
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({ 
+      triggerElement: spyEl, 
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당
+})
+
+
+// 올해가 몇 년도인지 계산
+const thisYear = document.querySelector('.this-year')
+thisYear.textContent = new Date().getFullYear()
